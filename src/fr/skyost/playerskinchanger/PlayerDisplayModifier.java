3f7faabd2c04c9e89536e962f472b1a1b52e6314
Hector Romero <hector.romero@fdvsolutions.com>
2015-02-09 13:43:13 -0300

    Adsjutment for diquemc

--- src/fr/skyost/playerskinchanger/PlayerDisplayModifier.java
+++ src/fr/skyost/playerskinchanger/PlayerDisplayModifier.java
@@ -46,158 +46,159 @@ import org.json.simple.parser.ParseException;
 
 public class PlayerDisplayModifier {
 
-	private static final String PROFILE_URL = "https://sessionserver.mojang.com/session/minecraft/profile/";
-	private static final int WORKER_THREADS = 4;
+    private static final String PROFILE_URL = "https://sessionserver.mojang.com/session/minecraft/profile/";
+    private static final int WORKER_THREADS = 4;
 
-	private ProtocolManager protocolManager;
-	private ConcurrentMap<String, String> skinNames = Maps.newConcurrentMap();
-	private ConcurrentMap<String, String> displayNames = Maps.newConcurrentMap();
+    private ProtocolManager protocolManager;
+    private ConcurrentMap<String, String> skinNames = Maps.newConcurrentMap();
+    private ConcurrentMap<String, String> displayNames = Maps.newConcurrentMap();
 
-	private Cache<String, String> profileCache = CacheBuilder.newBuilder().maximumSize(500).expireAfterWrite(4, TimeUnit.HOURS).build(new CacheLoader<String, String>() {
+    private Cache<String, String> profileCache = CacheBuilder.newBuilder().maximumSize(500).expireAfterWrite(4, TimeUnit.HOURS).build(new CacheLoader<String, String>() {
 
-		public String load(String name) throws Exception {
-			return getProfileJson(name);
-		};
-		
-	});
+        public String load(String name) throws Exception {
+            return getProfileJson(name);
+        }
 
-	public PlayerDisplayModifier(Plugin plugin) {
-		protocolManager = ProtocolLibrary.getProtocolManager();
-		protocolManager.getAsynchronousManager().registerAsyncHandler(new PacketAdapter(plugin, ListenerPriority.NORMAL,
-		// Packet we are modifying
-		PacketType.Play.Server.NAMED_ENTITY_SPAWN,
+        ;
 
-		// Packets that must be sent AFTER the entity spawn packet
-		PacketType.Play.Server.ENTITY_EFFECT, PacketType.Play.Server.ENTITY_EQUIPMENT, PacketType.Play.Server.ENTITY_METADATA, PacketType.Play.Server.UPDATE_ATTRIBUTES, PacketType.Play.Server.ATTACH_ENTITY, PacketType.Play.Server.BED) {
+    });
 
-			// This will be executed on an asynchronous thread
-			@Override
-			public void onPacketSending(PacketEvent event) {
-				// We only care about the entity spawn packet
-				if(event.getPacketType() != PacketType.Play.Server.NAMED_ENTITY_SPAWN) {
-					return;
-				}
+    public PlayerDisplayModifier(Plugin plugin) {
+        protocolManager = ProtocolLibrary.getProtocolManager();
+        protocolManager.getAsynchronousManager().registerAsyncHandler(new PacketAdapter(plugin, ListenerPriority.NORMAL,
+                // Packet we are modifying
+                PacketType.Play.Server.NAMED_ENTITY_SPAWN,
 
-				Player toDisplay = (Player)event.getPacket().getEntityModifier(event).read(0);
-				String skinName = skinNames.get(toDisplay.getName());
-				String displayName = displayNames.get(toDisplay.getName());
+                // Packets that must be sent AFTER the entity spawn packet
+                PacketType.Play.Server.ENTITY_EFFECT, PacketType.Play.Server.ENTITY_EQUIPMENT, PacketType.Play.Server.ENTITY_METADATA, PacketType.Play.Server.UPDATE_ATTRIBUTES, PacketType.Play.Server.ATTACH_ENTITY, PacketType.Play.Server.BED) {
 
-				if(skinName == null && displayName == null) {
-					return;
-				}
-				StructureModifier<WrappedGameProfile> profiles = event.getPacket().getGameProfiles();
-				WrappedGameProfile original = profiles.read(0);
-				WrappedGameProfile result = new WrappedGameProfile(extractUUID(original.getName()), displayName != null ? displayName : original.getName());
+            // This will be executed on an asynchronous thread
+            @Override
+            public void onPacketSending(PacketEvent event) {
+                // We only care about the entity spawn packet
+                if (event.getPacketType() != PacketType.Play.Server.NAMED_ENTITY_SPAWN) {
+                    return;
+                }
 
-				updateSkin(result, skinName != null ? skinName : result.getName());
-				profiles.write(0, result);
-			}
-		}).start(WORKER_THREADS);
-	}
+                Player toDisplay = (Player) event.getPacket().getEntityModifier(event).read(0);
+                String skinName = skinNames.get(toDisplay.getName());
+                String displayName = displayNames.get(toDisplay.getName());
 
-	private UUID extractUUID(final String playerName) {
-		return Bukkit.getOfflinePlayer(playerName).getUniqueId();
-	}
+                if (skinName == null && displayName == null) {
+                    return;
+                }
+                StructureModifier<WrappedGameProfile> profiles = event.getPacket().getGameProfiles();
+                WrappedGameProfile original = profiles.read(0);
+                WrappedGameProfile result = new WrappedGameProfile(extractUUID(original.getName()), displayName != null ? displayName : original.getName());
 
-	// This will be cached by Guava
-	private String getProfileJson(String name) throws IOException {
-		final URL url = new URL(PROFILE_URL + extractUUID(name).toString().replace("-", "") + "?unsigned=false");
-		final URLConnection uc = url.openConnection();
+                updateSkin(result, skinName != null ? skinName : result.getName());
+                profiles.write(0, result);
+            }
+        }).start(WORKER_THREADS);
+    }
 
-		return CharStreams.toString(new InputSupplier<InputStreamReader>() {
+    private UUID extractUUID(final String playerName) {
+        return Bukkit.getOfflinePlayer(playerName).getUniqueId();
+    }
 
-			@Override
-			public InputStreamReader getInput() throws IOException {
-				return new InputStreamReader(uc.getInputStream(), Charsets.UTF_8);
-			}
-		});
-	}
+    // This will be cached by Guava
+    private String getProfileJson(String name) throws IOException {
+        final URL url = new URL(PROFILE_URL + extractUUID(name).toString().replace("-", "") + "?unsigned=false");
+        final URLConnection uc = url.openConnection();
 
-    private JSONObject getProfileJSON (String skinOwner) throws ExecutionException, ParseException{
+        return CharStreams.toString(new InputSupplier<InputStreamReader>() {
+
+            @Override
+            public InputStreamReader getInput() throws IOException {
+                return new InputStreamReader(uc.getInputStream(), Charsets.UTF_8);
+            }
+        });
+    }
+
+    private JSONObject getProfileJSON(String skinOwner) throws ExecutionException, ParseException {
         String json = PlayerSkinChanger.getPlugin().getProfileCache(skinOwner);
-        if(json == null){
+        if (json == null) {
             json = profileCache.get(skinOwner);
             PlayerSkinChanger.getPlugin().setProfileCache(skinOwner, json);
         }
-        return (JSONObject)new JSONParser().parse(json);
+        return (JSONObject) new JSONParser().parse(json);
 
     }
-	private void updateSkin(WrappedGameProfile profile, String skinOwner) {
-		try {
 
-			JSONObject json = getProfileJSON(skinOwner);
-			JSONArray properties = (JSONArray)json.get("properties");
+    private void updateSkin(WrappedGameProfile profile, String skinOwner) {
+        try {
 
-			for(int i = 0; i < properties.size(); i++) {
-				JSONObject property = (JSONObject)properties.get(i);
-				String name = (String)property.get("name");
-				String value = (String)property.get("value");
-				String signature = (String)property.get("signature"); // May be NULL
+            JSONObject json = getProfileJSON(skinOwner);
+            JSONArray properties = (JSONArray) json.get("properties");
 
-				// Uncomment for ProtocolLib 3.4.0
-				profile.getProperties().put(name, new WrappedSignedProperty(name, value, signature));
-//				((GameProfile)profile.getHandle()).getProperties().put(name, new Property(name, value, signature));
-			}
-		}
-		catch(Exception e) {
-			// ProtocolLib will throttle the number of exceptions printed to the console log
-            Bukkit.getLogger().info("Error fetching player profile: " + skinOwner);
-			throw new RuntimeException("Cannot fetch profile for " + skinOwner, e);
-		}
-	}
+            for (int i = 0; i < properties.size(); i++) {
+                JSONObject property = (JSONObject) properties.get(i);
+                String name = (String) property.get("name");
+                String value = (String) property.get("value");
+                String signature = (String) property.get("signature"); // May be NULL
 
-	public void changeDisplay(String string, String toSkin) {
-		changeDisplay(string, toSkin, null);
-	}
+                // Uncomment for ProtocolLib 3.4.0
+                profile.getProperties().put(name, new WrappedSignedProperty(name, value, signature));
+//                ((GameProfile)profile.getHandle()).getProperties().put(name, new Property(name, value, signature));
+            }
+        } catch (Exception e) {
+            // ProtocolLib will throttle the number of exceptions printed to the console log
+            throw new RuntimeException("Cannot fetch profile for " + skinOwner, e);
+        }
+    }
 
-	public void changeDisplay(Player player, String toSkin, String toName) {
-		if(updateMap(skinNames, player.getName(), toSkin) | updateMap(displayNames, player.getName(), toName)) {
-			refreshPlayer(player);
-		}
-	}
+    public void changeDisplay(String string, String toSkin) {
+        changeDisplay(string, toSkin, null);
+    }
 
-	public void changeDisplay(String playerName, String toSkin, String toName) {
-		if(updateMap(skinNames, playerName, toSkin) | updateMap(displayNames, playerName, toName)) {
-			refreshPlayer(playerName);
-		}
-	}
+    public void changeDisplay(Player player, String toSkin, String toName) {
+        if (updateMap(skinNames, player.getName(), toSkin) | updateMap(displayNames, player.getName(), toName)) {
+            refreshPlayer(player);
+        }
+    }
 
-	public void removeChanges(Player player) {
-		changeDisplay(player.getName(), null, null);
-	}
+    public void changeDisplay(String playerName, String toSkin, String toName) {
+        if (updateMap(skinNames, playerName, toSkin) | updateMap(displayNames, playerName, toName)) {
+            refreshPlayer(playerName);
+        }
+    }
 
-	public void removeChanges(String playerName) {
-		changeDisplay(playerName, null, null);
-	}
+    public void removeChanges(Player player) {
+        changeDisplay(player.getName(), null, null);
+    }
 
-	/**
-	 * Update the map with the new key-value pair.
-	 * @param map - the map.
-	 * @param key - the key of the pair.
-	 * @param value - the new value, or NULL to remove the pair.
-	 * @return TRUE if the map was updated, FALSE otherwise.
-	 */
-	private <T, U> boolean updateMap(Map<T, U> map, T key, U value) {
-		if(value == null) {
-			return map.remove(key) != null;
-		}
-		else {
-			return !Objects.equal(value, map.put(key, value));
-		}
-	}
+    public void removeChanges(String playerName) {
+        changeDisplay(playerName, null, null);
+    }
 
-	private void refreshPlayer(String name) {
-		Player player = Bukkit.getPlayer(name);
+    /**
+     * Update the map with the new key-value pair.
+     *
+     * @param map   - the map.
+     * @param key   - the key of the pair.
+     * @param value - the new value, or NULL to remove the pair.
+     * @return TRUE if the map was updated, FALSE otherwise.
+     */
+    private <T, U> boolean updateMap(Map<T, U> map, T key, U value) {
+        if (value == null) {
+            return map.remove(key) != null;
+        } else {
+            return !Objects.equal(value, map.put(key, value));
+        }
+    }
 
-		if(player != null) {
-			refreshPlayer(player);
-		}
-	}
+    private void refreshPlayer(String name) {
+        Player player = Bukkit.getPlayer(name);
 
-	private void refreshPlayer(Player player) {
-		// Refresh the displayed entity
-		// Refresh the displayed entity
-		protocolManager.updateEntity(player, protocolManager.getEntityTrackers(player));
-	}
+        if (player != null) {
+            refreshPlayer(player);
+        }
+    }
+
+    private void refreshPlayer(Player player) {
+        // Refresh the displayed entity
+        // Refresh the displayed entity
+        protocolManager.updateEntity(player, protocolManager.getEntityTrackers(player));
+    }
 
 }
